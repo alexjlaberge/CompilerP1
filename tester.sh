65a74feb76fa49e6d7b71e81e8221d954559b024
Alec Ten Harmsel <alec@alectenharmsel.com>
2015-01-15 18:47:47 -0500

    Update testing script

--- tester.sh
+++ tester.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # Proper header for a Bash script.
-./dcc < samples/$1.frag > output.txt
-diff output.txt samples/$1.out
-echo DONE
+
+for input in `ls samples/*.frag samples/*.decaf | grep -v "/bad"`
+do
+        echo -ne "Testing ${input}..."
+        ./dpp < ${input} | ./dcc | diff -q \
+                `echo ${input} | sed -e "s/\..*/\.out/"` - && echo
+done
