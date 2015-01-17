826d350fb03acfd6799ec3be84b84a39f205fca5
Alec Ten Harmsel <alec@alectenharmsel.com>
2015-01-16 20:55:16 -0500

    Minor testing script update

--- tester.sh
+++ tester.sh
@@ -4,6 +4,6 @@
 for input in `ls samples/*.frag samples/*.decaf | grep -v "/bad"`
 do
         echo -ne "Testing ${input}..."
-        ./dpp < ${input} | ./dcc | diff -q \
-                `echo ${input} | sed -e "s/\..*/\.out/"` - && echo
+        ./dcc < ${input} | diff -q \
+                `echo ${input} | sed -e "s/\..*/\.out/"` - && echo "PASS"
 done
